e7c9aece7efac0f753116a1b43dfafd37462ac6a
Esteban Sepulveda <tebansd@icloud.com>
2023-07-06 21:58:32 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 class Casino {
-    // Write code here
-};
+    constructor (name){
+    this.timesPlayed = 0;    
+}
 
+playGame(betAmount){
+    if (Math.random()<= .5 ){
+        console.log(this.name + ' wins!')
+    }
+    else{
+        console.log('The player wins' + betAmount)
+    }
+}
+};
 // TESTS
 const myCasino = new Casino("HackerU Casino");
 console.log(myCasino);
@@ -11,20 +21,21 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
-const myBonusCasino = new Casino("HackerU Bonus Casino", true);
-console.log(myBonusCasino);
-myBonusCasino.playGame(5);
-myBonusCasino.playGame(15);
-myBonusCasino.playGame(25);
-myBonusCasino.playGame(35);
-*/
+// /*
+// const myBonusCasino = new Casino("HackerU Bonus Casino", true);
+// console.log(myBonusCasino);
+// myBonusCasino.playGame(5);
+// myBonusCasino.playGame(15);
+// myBonusCasino.playGame(25);
+// myBonusCasino.playGame(35);
+// */
 
 // Extra BONUS TESTS
-/*
-const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
-console.log(myExtraBonusCasino);
-myExtraBonusCasino.rollDie(6);
-myExtraBonusCasino.rollDie(20);
-myExtraBonusCasino.rollDie(100);
-*/
+// /*
+// const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
+// console.log(myExtraBonusCasino);
+// myExtraBonusCasino.rollDie(6);
+// myExtraBonusCasino.rollDie(20);
+// myExtraBonusCasino.rollDie(100);
+// */
+
